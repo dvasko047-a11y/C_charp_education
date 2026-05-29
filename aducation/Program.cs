6af89bd3f96357ae8e4741aa7c823f77847d187c
dvasko047 <dvasko047@gmail.com>
2026-05-29 21:36:04 +0300

    first comment in studio

---
+++ aducation/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace aducation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+
+            int a;
+            a = 10;
+            int b = 20;
+            int c;
+            c = a + b;
+            Console.WriteLine("c={0}", c);
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
